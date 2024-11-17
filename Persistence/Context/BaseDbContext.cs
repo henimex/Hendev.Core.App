6e6c7f3b6f8999293842450e061546aec48e883f
henimex <henimex@gmail.com>
2024-11-18 00:39:50 +0300

    Add project files.

---
+++ Persistence/Context/BaseDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Context;
+
+public class BaseDbContext : DbContext
+{
+    public IConfiguration Configuration { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+
+    public BaseDbContext(DbContextOptions contextOptions, IConfiguration configuration) : base(contextOptions)
+    {
+        Configuration = configuration;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Application.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    {
+        services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("HendevCore"));
+        services.AddScoped<IBrandRepository, BrandRepository>();
+
+        return services;
+    }
+}
